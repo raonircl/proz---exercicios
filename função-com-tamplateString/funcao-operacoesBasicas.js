e95373f45d37b06cf8158e4404715793cd24a3d6
raonircl <raoni.afi@gmail.com>
2023-12-08 12:12:50 -0300

    feat: função com template string

---
+++ função-com-tamplateString/funcao-operacoesBasicas.js
@@ -0,0 +1,13 @@
+function operacoesBasicas(num1, num2) {
+    const soma = num1 + num2;
+    const subtracao = num1 - num2;
+    const multiplicacao = num1 * num2;
+    const divisao = num1 / num2;
+
+    console.log(`${num1} + ${num2} = ${soma}`);
+    console.log(`${num1} - ${num2} = ${subtracao}`);
+    console.log(`${num1} x ${num2} = ${multiplicacao}`);
+    console.log(`${num1} / ${num2} = ${divisao}`);
+}
+
+operacoesBasicas(4, 5);
